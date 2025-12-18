1f4f393af30cb7ad048cfb732327cc385b14207c
ŞENAY AKAGÜNDÜZ <senayakagunduz@192.168.1.103>
2025-12-18 12:15:42 +0300

    music-app

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Card from './components/MusicCard';
+import HomeScreen from './Screens/HomeScreen';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <HomeScreen/>
   );
 }
 
---
+++ types/music.ts
@@ -0,0 +1,9 @@
+export interface IMusic {
+  id: string;
+  title: string;
+  artist: string;
+  album: string;
+  year: number;
+  isSoldOut: boolean;
+  imageUrl: string;
+}
